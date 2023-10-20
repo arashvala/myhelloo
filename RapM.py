7cdc29895d6ea34e81978a6c23a718b5890893e2
arashvala <122074342+arashvala@users.noreply.github.com>
2023-10-20 09:35:03 +0330

    Update RapM.py

--- RapM.py
+++ RapM.py
@@ -1,11 +1,32 @@
 import streamlit as st
-st.write("Hiiiiiiiiiii SHABNAM")
-
-
-import streamlit as st
-import pandas as pd
 import numpy as np
+import pandas as pd
 
-chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+
+st.write("Hiiiiiiiiiii")
+
+
+with st.sidebar:
+    st.title('Train Dynamic Simulation')
+    st.image(
+        "https://www.euspa.europa.eu/sites/default/files/files/content/news/images/home/alstom.jpg",
+        width=280,  # Manually Adjust the width of the image as per requirement
+    )
+    st.header('Please Enter The Input Data:')
+
+with st.sidebar:
+    F1 = st.number_input('Insert First Frequency:', min_value=1, max_value=40, step=1)
+
+with st.sidebar:
+    F2 = st.number_input('Insert Second Frequency:', min_value=1, max_value=40, step=1)
+
+with st.sidebar:
+    Time = st.number_input('Insert Time:', min_value=1, max_value=100, step=1)
+
+
+Xt= np.arange(0, Time, 0.01)
+Yt= np.sin(F1*Xt)+np.sin(F2*Xt)
+
+chart_data = pd.DataFrame(Yt)
 
 st.area_chart(chart_data)
