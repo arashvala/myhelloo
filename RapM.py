0ef5d2d44d6ffb0e2674c9ac9879bb2029147ed6
arashvala <122074342+arashvala@users.noreply.github.com>
2023-10-20 09:12:53 +0330

    Update RapM.py

--- RapM.py
+++ RapM.py
@@ -2,13 +2,10 @@ import streamlit as st
 st.write("Hiiiiiiiiiii SHABNAM")
 
 
-with st.sidebar:
-    st.title('Train Dynamic Simulation')
-    st.image(
-        "https://www.euspa.europa.eu/sites/default/files/files/content/news/images/home/alstom.jpg",
-        width=280,  # Manually Adjust the width of the image as per requirement
-    )
-    st.header('Please Enter The Input Data:')
-  
-number = st.slider("Pick a number: ", min_value=1, max_value=10)
-st.text("Your number is " + str(number))
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+
+st.area_chart(chart_data)
